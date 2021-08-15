f3b672508cf519cfc3d5e9b72e3ab0cdfb79606f
Kezia Campos <keziacamposcs@gmail.com>
2021-08-15 16:39:00 -0300

    atualização 15.08.21

--- Aula 09.08.21/ex1b.java
+++ Aula 09.08.21/ex1b.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class ex1b {
     public static void main(String[] args) {
         //Variaveis
--- Aula 09.08.21/ex1c.java
+++ Aula 09.08.21/ex1c.java
@@ -1,9 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class ex1c {
     public static void main(String[] args) {
         //Variaveis
         String aux ="";
         float peso;
         float altura;
+        float imc;
 
         aux = JOptionPane.showInputDialog(null, "Peso: ");
         peso = Float.parseFloat(aux);
@@ -11,13 +14,25 @@ public class ex1c {
         aux = JOptionPane.showInputDialog(null, "Altura: ");
         altura = Float.parseFloat(aux);
 
-        float imc = peso / (altura*altura);
+        imc = peso / (altura*altura);
 
         if (imc<18.5){
-            JOptionPane.showMessageDialog(null, "\nIMC: "+imc);        
+            JOptionPane.showMessageDialog(null, "Abaixo do normal\nIMC: "+imc);        
         }
-        else if(imc>=18.5){
-            
+        else if(imc>=18.5 && imc<=24.4){
+            JOptionPane.showMessageDialog(null, "Ideal\nIMC: "+imc);        
+        }
+        else if(imc>=18.5 && imc<=24.4){
+            JOptionPane.showMessageDialog(null, "Pré-obesidade\nIMC: "+imc);        
+        }
+        else if(imc>=30 && imc<=34.9){
+            JOptionPane.showMessageDialog(null, "Obesidade classe I\nIMC: "+imc);        
+        }
+        else if(imc>=35 && imc<=39.9){
+            JOptionPane.showMessageDialog(null, "Obesidade classe II\nIMC: "+imc);        
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Obesidade classe III\nIMC: "+imc);        
         }
     }
 }
