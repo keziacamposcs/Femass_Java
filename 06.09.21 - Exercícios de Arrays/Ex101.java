56f7ffd71e4e0769b370ec7079aece21b2a0338b
Kezia Campos <keziacamposcs@gmail.com>
2021-09-20 16:43:01 -0300

    20.09.21

---
+++ 06.09.21 - Exercícios de Arrays/Ex101.java
@@ -0,0 +1,21 @@
+public class Ex101
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<ArrayList<Integer>> matriz = new ArrayList<>();
+    //    ArrayList<Aluno> array_aluno = new ArrayList<Aluno>();
+
+        //Matriz
+        for(int linha=0; linha<50; linha++)
+        {
+            for(int coluna=0; coluna<20; coluna++)
+            {
+                Mtx matriz = new Mtx();
+
+                matriz.add(JOptionPane.showInputDialog("Digite um numero - sem ser negativo - para o tamanho da lista: "));
+        }
+        //Fim Matriz
+
+        System.out.println(array_aluno);
+    }       
+}
