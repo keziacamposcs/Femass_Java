c00e26dcc60336b8f123553baf34d381c8a36f0a
Kezia Campos <keziacamposcs@gmail.com>
2021-09-26 10:21:28 -0300

    26.09.21

--- 27.09.21 - Exercícios de Exceção/ex1.java
+++ 27.09.21 - Exercícios de Exceção/ex1.java
@@ -12,10 +12,41 @@ public class ex1 {
     public static void main(String[] args) {  
         try 
         {
-            x = 10;
-            int n = 5 + “5”;
+            int x = 10;
+        }
+
+        catch(Exception erro)
+        {
+            System.out.println(erro.getMessage());
+            erro.printStackTrace();
+        }
+
+        try 
+        {
+            String n;
+            n = 5 + "5";
+        }
+
+        catch(Exception erro)
+        {
+            System.out.println(erro.getMessage());
+            erro.printStackTrace();
+        }
+
+        try 
+        {
             int [] nr = new int[2];
             nr[2] = 10;
+        }
+
+        catch(Exception erro)
+        {
+            System.out.println(erro.getMessage());
+            erro.printStackTrace();
+        }
+
+        try 
+        {
             float r = 10 / 0;
         }
 
--- 27.09.21 - Exercícios de Exceção/ex2.java
+++ 27.09.21 - Exercícios de Exceção/ex2.java
@@ -1,16 +1,33 @@
 import javax.swing.JOptionPane;
 
 public class ex2 {
-    public static void main(String[] args) {  
-        try 
-        {
-            int nr1 = Integer.parseInt((JOptionPane.showInputDialog("Numero 1: "))]);
-            int nr2 = Integer.parseInt((JOptionPane.showInputDialog("Numero 1: "))]);
+    public static void main(String[] args) {
 
-            float resultado = nr1 / nr2;
+        boolean done = false;
 
-            JOptionPane.showMessageDialog(null, resultado);
-        }
+        while(!done)
+            try 
+            {
+                int nr1 = Integer.parseInt((JOptionPane.showInputDialog("Numero 1: ")));
+                int nr2 = Integer.parseInt((JOptionPane.showInputDialog("Numero 2: ")));
+                float resultado;
+                resultado = nr1 / nr2;
+
+                JOptionPane.showMessageDialog(null, resultado);
+
+                done = true;
+            }
+
+            catch (NumberFormatException erro)
+            {
+                JOptionPane.showMessageDialog(null, "Digite apenas numeros inteiros." + " O texto do erro e: " + erro.toString(), "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+            
+            catch (ArithmeticException erro)
+            {
+                JOptionPane.showMessageDialog(null, "O denomidador nao pode ser 0!" + " O texto do erro e: " + erro.toString());
+            }
+        
 
     }
 }
